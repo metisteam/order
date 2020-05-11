8e91941da9fe2f5bc1b63b48caae9c161a344eca
冬字差一人 <329636491@qq.com>
2020-05-11 14:55:15 +0800

    修改订单接口

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -8,12 +8,10 @@ class OrderController extends Controller
     {
     	return $this->success([
     			'pingList' => [
-    				['id' => 1, 'avatar' => 'https://img.yzcdn.cn/vant/cat.jpeg', 'name' => '天下第一暗器', 'time' => 30 * 60 * 60 * 1000],
-    				['id' => 2, 'avatar' => 'https://img.yzcdn.cn/vant/cat.jpeg', 'name' => '天下第一暗器', 'time' => 10 * 60 * 60 * 1000],
-    				['id' => 3, 'avatar' => 'https://img.yzcdn.cn/vant/cat.jpeg', 'name' => '天下第一暗器', 'time' => 20 * 60 * 60 * 1000]
+    				['id' => 1, 'avatar' => 'https://img.yzcdn.cn/vant/cat.jpeg', 'name' => '天下第一暗器' . rand(0 , 10), 'time' => 30 * 60 * 60 * 1000],
+    				['id' => 2, 'avatar' => 'https://img.yzcdn.cn/vant/cat.jpeg', 'name' => '天下第一暗器'. rand(0 , 10), 'time' => 10 * 60 * 60 * 1000],
+    				['id' => 3, 'avatar' => 'https://img.yzcdn.cn/vant/cat.jpeg', 'name' => '天下第一暗器'. rand(0 , 10), 'time' => 20 * 60 * 60 * 1000]
     			],
-    			'animation' => false,
-    			'srcImage' => 'https://img.huxiucdn.com/article/cover/202004/23/204858345271.jpg'
     	]);
     }
 }
